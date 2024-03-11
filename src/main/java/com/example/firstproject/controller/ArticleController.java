a179f28bc73eb8d2173239a231f0b2b0b322d506
bagjinhwan <spdlqjxp@gmail.com>
2024-03-11 16:40:33 +0900

    링크 및 리다이렉트

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -36,7 +36,7 @@ public class ArticleController {
         Article saved = articleRepository.save(article);
         log.info(saved.toString());
 //        System.out.println(saved.toString());
-        return "";
+        return "redirect:/articles/" + saved.getId(); //리다이렉트
     }
 
     @GetMapping("/articles/{id}") // 데이터 조회 접수 요청
