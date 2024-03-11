6eb3e9c8e9bbc58ea1b7eb1c85bbb77736202700
bagjinhwan <spdlqjxp@gmail.com>
2024-03-11 15:39:23 +0900

    단일 데이터 조회

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
+
     @GetMapping("/articles/new")
     public String newArticleForm() {
         return "articles/new";
@@ -33,4 +36,16 @@ public class ArticleController {
 //        System.out.println(saved.toString());
         return "";
     }
+
+    @GetMapping("/articles/{id}") // 데이터 조회 접수 요청
+    public String show(@PathVariable Long id, Model model) { //매개변수로 id 가져오기
+        log.info("id = " + id);
+//      1. id를 조회해 db에서 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+//      2. 모델에서 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+//      3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
 }
