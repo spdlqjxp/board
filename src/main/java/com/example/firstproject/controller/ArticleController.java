4f91e2c53a220baec68151747abd7421e07aa067
bagjinhwan <spdlqjxp@gmail.com>
2024-03-13 01:00:45 +0900

    데이터 삭제

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -86,5 +87,20 @@ public class ArticleController {
         }
 //        3.수정결과 페이지 리다이렉트
         return "redirect:/articles/" + articleEntity.getId();
+    }
+
+    @GetMapping("articles/{id}/delete")
+    public String delete(@PathVariable Long id, RedirectAttributes rttr) {
+        log.info("Request Deleting");
+//        1.삭제 대상 가져오기
+        Article target = articleRepository.findById(id).orElse(null);
+        log.info(target.toString());
+//        2.대상 엔티티 삭제하기
+        if (target != null) {
+            articleRepository.delete(target);
+            rttr.addFlashAttribute("msg", "Delete Complete!");
+        }
+//        3.결과 페이지로 리다이렉트
+        return "redirect:/articles";
     }
 }
